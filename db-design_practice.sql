69cb341fcf1a3552e9a1875aa6ce47f5deaccfd4
stacey.tadrus@gmail.com <stacey.tadrus@gmail.com>
2025-06-27 10:07:33 -0500

    Create db-design_practice.sql

---
+++ db-design_practice.sql
@@ -0,0 +1,49 @@
+START TRANSACTION;
+
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS images;
+DROP TABLE IF EXISTS users;
+
+
+CREATE TABLE users (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(50) NOT NULL DEFAULT 'N/A',
+  email VARCHAR(100) NULL,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE posts (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  user_id INT NOT NULL REFERENCES users(id),
+  title VARCHAR(100) NOT NULL,
+  content TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE images (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  user_id INT NOT NULL REFERENCES users(id),
+  title VARCHAR(100) NOT NULL,
+  content TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO users (id, username, email, password) VALUES 
+(1,'alice', 'alice@test.com', 'password123'),
+(2, 'bob', 'bob@test.com', 'password456'),
+(3, 'charlie', 'charlie@test.com', 'password789');
+
+INSERT INTO posts (user_id, title, content) VALUES
+(1, 'First Post', 'This is the content of the first post'),
+(2, 'Second Post', 'This is the content of the second post'),
+(3, 'Third Post', 'This is the content of the third post'),
+(1, 'Fourth Post', 'This is the content of the fourth post'),
+(2, 'Fifth Post', 'This is the content of the fifth post');
+
+INSERT INTO images (user_id, title, content) VALUES
+(1, 'First Image', 'This is the content of the first image'),
+(2, 'Second Image', 'This is the content of the second image'),
+(3, 'Third Image', 'This is the content of the third image'),
+(1, 'Fourth Image', 'This is the content of the fourth image'),
+(2, 'Fifth Image', 'This is the content of the fifth image');
+
+COMMIT;
