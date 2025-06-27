9ab17930749e5c0e50a910d18cfb35a6c15b6a2a
stacey.tadrus@gmail.com <stacey.tadrus@gmail.com>
2025-06-27 10:14:09 -0500

    added comments to sql and readme

--- db-design_practice.sql
+++ db-design_practice.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS images;
 DROP TABLE IF EXISTS users;
 
-
+#practice during lesson
 CREATE TABLE users (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   username VARCHAR(50) NOT NULL DEFAULT 'N/A',
@@ -12,6 +12,7 @@ CREATE TABLE users (
   password VARCHAR(255) NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+#practice during lesson
 CREATE TABLE posts (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   user_id INT NOT NULL REFERENCES users(id),
@@ -19,6 +20,7 @@ CREATE TABLE posts (
   content TEXT NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+#created images table using data from the posts table. 
 CREATE TABLE images (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   user_id INT NOT NULL REFERENCES users(id),
@@ -38,12 +40,12 @@ INSERT INTO posts (user_id, title, content) VALUES
 (3, 'Third Post', 'This is the content of the third post'),
 (1, 'Fourth Post', 'This is the content of the fourth post'),
 (2, 'Fifth Post', 'This is the content of the fifth post');
-
+#added to complete lesson practice 
 INSERT INTO images (user_id, title, content) VALUES
 (1, 'First Image', 'This is the content of the first image'),
 (2, 'Second Image', 'This is the content of the second image'),
 (3, 'Third Image', 'This is the content of the third image'),
 (1, 'Fourth Image', 'This is the content of the fourth image'),
 (2, 'Fifth Image', 'This is the content of the fifth image');
-
+#used rollback to test the data then commit with no errors
 COMMIT;
